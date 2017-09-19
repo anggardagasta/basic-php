c3bdc1dbce731fdea5b79c3129a5e1533c704ba8
anggardagasta <anggarda.gasta.p@gmail.com>
2017-09-19 19:34:21 +0700

    Create bilanganPrima.php

---
+++ bilanganPrima.php
@@ -0,0 +1,19 @@
+<?php
+
+function bilanganPrima($n)
+{
+  for ($i = 1; $i<=$n; $i++) {
+    $counter = 0;
+    for ($j=1;$j<=$i;$j++){
+      if($i % $j==0) {
+        $counter++;
+      }
+    } 
+    if($counter==2){
+      echo "{$i}\n";
+    }
+  }
+} 
+
+//prima 1 - 100
+bilanganPrima(100);
